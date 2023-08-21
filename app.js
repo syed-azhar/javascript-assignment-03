4bf07f60a865d08dd44af81731f33f4aeb10d577
syed-azhar <93089810+syed-azhar@users.noreply.github.com>
2023-08-20 22:49:43 -0700

    javascript-assignment 03

---
+++ app.js
@@ -0,0 +1,102 @@
+// > ------------------- ARRAY ASSIGNMENT 03 ---------------------- <br // 
+
+
+// Write a program to store 3 student names in an array.Take
+// another array to store score of these three students.
+// Assume that total marks are 500 for each student, display
+// the scores & percentages of students.
+
+
+// var students = ["bilal", "Ahmed", "Zubair"]
+// var scoree = [320,230,480]
+// var totalMarks = 500;
+
+// document.write(
+//       "Score of " + students [0] + " is " + scoree[0] + " .percentage: " + (scoree[0] / 500) *100+ " %"
+// );
+
+// document.write(
+//     " <br> score of " + students [1] + " is " + scoree[1] + " percentage " + (scoree[1]/500) * 100 + " %"
+// );
+// document.write(
+//     "  </br> Score of " + students[2] + " is " + scoree[2] + " Percentage " + (scoree[2]/500) * 100 + " %" 
+// )
+
+
+// Write a program to store student scores in an array &
+// sort the array in ascending order using Array’s sort
+// method.
+
+
+// var studentScore = [240,120,302,520]
+
+// var result = studentScore.sort();
+
+// console.log(result);
+
+
+
+// 11. Write a program to initialize an array with city names.
+// Copy 3 array elements from cities array to selectedCities
+// array.
+
+
+
+// var cityNames = ["karachi","hyderabad","multan","lahore","queta"]
+
+// var result = cityNames.slice(0,3)
+
+// console.log(result)
+
+
+
+
+// 12. Write a program to create a single string from the
+// below mentioned array:
+// var arr = [“This ”, “ is ”, “ my ”, “ cat”];
+// (Use array’s join method)
+
+
+// var arr = ["This","is","my","cat"];
+
+// var result = arr.join(arr)
+
+// console.log(result)
+
+
+
+// 1. Declare an empty array using JS literal notation to store
+// student names in future.
+// 2. Declare an empty array using JS object notation to store
+// student names in future.
+// 3. Declare and initialize a strings array.
+// 4. Declare and initialize a numbers array.
+// 5. Declare and initialize a boolean array.
+// 6. Declare and initialize a mixed array.
+// 7. Declare and Initialize an array and store available
+// education qualifications in Pakistan (e.g. SSC, HSC, BCS,
+// BS, BCOM, MS, M. Phil., PhD). Show the listed
+// qualifications in your browser like:
+
+
+// var emptyArray = [""];
+
+// var emptyArray = new Array();
+
+// var stringArray = ["apple" , "banana", "mango"];
+
+// var numberArry = [1,2,3,4,5]
+
+// var booleanArray = [true,false,true]
+
+// var mixedArray = [1,3, "array" , true]
+
+// var storeEducation = ["SSC"," <br> HSC"," <br> BCS"," <br> BS"," <br> BCOM","  <br> MS"," <br> M.PHIL"," <br> PHD"]
+
+// document.write("<h2>Education Qualifications in Pakistan:</h2>");
+// document.write(storeEducation);
+
+
+
+
+
